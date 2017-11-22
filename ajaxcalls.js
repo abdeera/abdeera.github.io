94e196f020528b5b3604c16f0c0d16254accdc5b
Ezzouaoui Abderrahim <11490855+abdeera@users.noreply.github.com>
2017-11-22 06:51:46 +0100

    Update ajaxcalls.js

--- ajaxcalls.js
+++ ajaxcalls.js
@@ -2,18 +2,18 @@ function getuser(){
 
  $.ajax({
     method: 'GET',
-    url: "https://crossorigin.me/https://github.com/",
+    url: "https://crossorigin.me/https://api.github.com/user",
     headers: {
-     'Host': 'github.com',
+     'Host': 'api.github.com',
      'Connection': 'keep-alive',
      'Cache-Control':'max-age=0',		      
      'Upgrade-Insecure-Requests':'1',
-     'Accept':'text/html,',
+     'Accept':'application/json',
      'Accept-Language':'r-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4',
      'Accept-Encoding': 'gzip, deflate, sdch, br',
-     'Origin':'https://github.com',
+     'Origin':'https://api.github.com',
      'Referer':'https://github.com/',
-     'Content-Type': 'text/html,application/json;charset=UTF-8',
+     'Content-Type': 'text/html;application/json;charset=UTF-8',
      'DNT':'1'
      }		      
  
