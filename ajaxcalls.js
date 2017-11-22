c88b3ae08ee8eec50e295dd7f8b4b113590e403b
Ezzouaoui Abderrahim <11490855+abdeera@users.noreply.github.com>
2017-11-22 06:27:56 +0100

    Update ajaxcalls.js

--- ajaxcalls.js
+++ ajaxcalls.js
@@ -9,11 +9,7 @@ function getuser(){
      'Accept':'text/html,',
      'Accept-Language':'r-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4',
      'Content-Type': 'text/html,application/json;charset=UTF-8',
-     'Access-Control-Allow-Origin': '*',
-     'Access-Control-Allow-Methods': 'DELETE, HEAD, GET, OPTIONS, POST, PUT',
-     'Access-Control-Allow-Headers': 'Content-Type, Content-Range, Content-Disposition, Content-Description',
-     'Access-Control-Max-Age': '1728000'
-      }		      
+     }		      
  
    }).done(function(data) { 
    alert(data);
