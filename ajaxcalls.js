6756db51b4c524844bd374f40035319293f0223a
Ezzouaoui Abderrahim <11490855+abdeera@users.noreply.github.com>
2017-11-22 06:30:59 +0100

    Update ajaxcalls.js

--- ajaxcalls.js
+++ ajaxcalls.js
@@ -4,11 +4,16 @@ function getuser(){
     method: 'GET',
     url: "https://crossorigin.me/https://github.com/",
     headers: {
+     'Host': 'github.com',
+     'Connection': 'keep-alive',
      'Cache-Control':'max-age=0',		      
      'Upgrade-Insecure-Requests':'1',
      'Accept':'text/html,',
      'Accept-Language':'r-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4',
+     'Accept-Encoding': gzip, deflate, sdch, br
+
      'Content-Type': 'text/html,application/json;charset=UTF-8',
+     DNT: 1
      }		      
  
    }).done(function(data) { 
