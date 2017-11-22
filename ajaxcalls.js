a9f4c47f928784983affa428e7ef5e512594c418
Ezzouaoui Abderrahim <11490855+abdeera@users.noreply.github.com>
2017-11-22 06:34:44 +0100

    Update ajaxcalls.js

--- ajaxcalls.js
+++ ajaxcalls.js
@@ -11,7 +11,8 @@ function getuser(){
      'Accept':'text/html,',
      'Accept-Language':'r-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4',
      'Accept-Encoding': 'gzip, deflate, sdch, br',
-
+     'Origin':'https://github.com',
+     'Referer':'https://github.com/',
      'Content-Type': 'text/html,application/json;charset=UTF-8',
      'DNT':'1'
      }		      
