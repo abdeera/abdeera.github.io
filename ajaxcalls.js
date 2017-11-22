aecbf08e12e8e8aa433b97d9cf361be904847ed0
Ezzouaoui Abderrahim <11490855+abdeera@users.noreply.github.com>
2017-11-22 06:20:07 +0100

    Update ajaxcalls.js

--- ajaxcalls.js
+++ ajaxcalls.js
@@ -8,15 +8,13 @@ function getuser(){
      'Cache-Control':'max-age=0',
       'Upgrade-Insecure-Requests':'1',		      
      'Upgrade-Insecure-Requests':'1',
- -    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
       'Accept':'text/html,',
- -    'Accept-Language':'r-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4'	
       'Accept-Language':'r-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4',
- +    'Content-Type': 'application/json;charset=UTF-8',
- +    'Access-Control-Allow-Origin': '*',
- +    'Access-Control-Allow-Methods': 'DELETE, HEAD, GET, OPTIONS, POST, PUT',
- +    'Access-Control-Allow-Headers': 'Content-Type, Content-Range, Content-Disposition, Content-Description',
- +    'Access-Control-Max-Age': '1728000'
+     'Content-Type': 'application/json;charset=UTF-8',
+     'Access-Control-Allow-Origin': '*',
+     'Access-Control-Allow-Methods': 'DELETE, HEAD, GET, OPTIONS, POST, PUT',
+     'Access-Control-Allow-Headers': 'Content-Type, Content-Range, Content-Disposition, Content-Description',
+     'Access-Control-Max-Age': '1728000'
       }		      
  }
    }).done(function(data) { 
